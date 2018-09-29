cb1a5ffa45c19db3ac89be3c10f5a7d58c7b9c4c
numbra <penumbra420@gmail.com>
2018-09-28 22:23:37 -0400

    Init commit

---
+++ src/main/java/ui/layout/Main.java
@@ -0,0 +1,24 @@
+package ui.layout;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main implements Initializable {
+
+    @FXML
+    Button testButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        testButton.setText("Test");
+    }
+
+    @FXML
+    public void test() {
+        System.out.println("Test!");
+    }
+}
